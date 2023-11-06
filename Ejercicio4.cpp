31b32f08e028a941b8e09b14c9763b81fb9b6ba1
ddealsan <ddealsan@myuax.com>
2023-11-06 17:46:14 +0100

    Ejercicio 1

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -21,7 +21,7 @@ int main() {
         Excepcion();
     }
     catch (const MiExcepcion& e) {
-        cout << "Excepcion capturada: " << e.what() << endl;
+        cout << "Excepcion captureada: " << e.what() << endl;
     }
     return 0;
 }

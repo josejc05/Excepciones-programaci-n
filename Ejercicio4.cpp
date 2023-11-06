5af38d4d685cabc5e0f1a7f691e60f0b7737b11d
ddealsan <ddealsan@myuax.com>
2023-11-06 17:44:17 +0100

    Ejercicio 1

---
+++ Ejercicio4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<exception>
+#include<string>
+using namespace std;
+
+class MiExcepcion : public std::exception {
+private:
+    std::string mensaje;
+public:
+    explicit MiExcepcion(const std::string& msg) : mensaje(msg) {}
+    const char* what() const noexcept override {
+        return mensaje.c_str();
+    }
+};
+
+void Excepcion() {
+    throw MiExcepcion("Ha ocurrio un error en la funcion lanzaExcepcion");
+}
+int main() {
+    try {
+        Excepcion();
+    }
+    catch (const MiExcepcion& e) {
+        cout << "Excepcion capturada: " << e.what() << endl;
+    }
+    return 0;
+}
